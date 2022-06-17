934d766234e619bd738f19ec0fcc91242fdf23b3
Test User <luner-stone@mail.ru>
2022-06-17 14:18:14 +0300

    pass message hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Message from './Message';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PassTheMessage extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      inputValue:"",
+      lastMessage:""
+    }
+  }
+  handlerButton=()=>{
+    this.setState({...this.state, lastMessage: this.state.inputValue, inputValue: ""})      
+  }
+  render(){
+    const formStyles = {
+      backgroundColor:"white",
+      padding: "2rem",
+      marginTop:"5rem"
+    }
+    return(
+      <>
+      <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-6" style={formStyles}>    
+        <h1>A Message You Would Like To Pass</h1>
+        <label htmlFor="message" className="form-label">Write your Message here</label>
+        <input name="message" value={this.state.inputValue} 
+        onChange={(event)=>this.setState({...this.state,inputValue:event.target.value})} 
+        className="form-control"/>
+        <button className="btn-primary" onClick={this.handlerButton}>Send</button>
+        <div></div>
+        <Message data={this.state.lastMessage}/>
+        </div>         
+        </div>
+        </>
+    )
+  }
 }
 
-export default App;
+export default PassTheMessage;
+
+/*class PassTheMessage extends React.Component{
+  constructor(){
+    super()
+    this.state = {inputValue: "",
+                  message:""
+  }
+    
+  }
+  render(){
+    return(
+      <>
+      <div class="container">
+  <div class="row">
+    <div class="col-3">
+     
+    </div>
+    <div class="col-6">
+      <h1>A Meassage You Would like to Pass</h1>
+      <label for="input" >Your Message</label>
+    <input onChange={(event)=>this.setState({inputValue:event.target.value})} value = {this.state.inputValue} type="text" className="form-control" id="input" placeholder="your-message"/>
+    <button onClick={()=>} type="button" className="btn btn-primary">Submit</button>
+    <h2>Last Message Delivered</h2>
+    <h2>Message Passed</h2>
+    </div>
+    <div class="col-3">
+    </div>
+  </div>
+</div>
+      </>
+    )
+  }
+}*/
---
+++ src/Message.jsx
@@ -0,0 +1,10 @@
+function Message({data}){
+    return(
+        <>
+        <h3>Last Message Delivered</h3>
+        <h4>{data}</h4>
+        </>
+    )
+}
+
+export default Message
